620bf39b35de0338d595a6d216382073353c3f0e
Aravinth-Altrai <aravinth-altrai@outlook.com>
2025-05-27 12:01:14 +0530

    offer1 comp modified

--- src/components/Offer1.jsx
+++ src/components/Offer1.jsx
@@ -4,8 +4,9 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { UndoDot } from 'lucide-react';
 import { Wrench,CircleGauge,Hammer, History, Brush,  Package } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {changeToUrl} from "../constants/constants"
+import image1 from "../assets/whatwedo/main.png"
 
 
 
@@ -54,42 +55,16 @@ const content = [
   
 
 const Offer1 = () => {
-  const sectionRef = useRef(null);
-  const { ref: inViewRef, inView } = useInView({ threshold: 0.5 });
-  const controls = useAnimation();
-  const titleControls = useAnimation();
-  const itemsControls = useAnimation();
+  
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (inView) {
-      controls.start({ opacity: 1, transition: { duration: 1.5, ease: 'easeOut' } });
-      titleControls.start({ x: 0, opacity: 1, transition: { duration: 1, ease: 'easeOut' } });
-      itemsControls.start((i) => ({
-        x: 0,
-        opacity: 1,
-        transition: { duration: 1, delay: i * 0.2, ease: 'easeOut' },
-      }));
-    } else {
-      controls.start({ opacity: 0 });
-      titleControls.start({ x: 50, opacity: 0 });
-      itemsControls.start((i) => ({
-        x: 50,
-        opacity: 0,
-      }));
-    }
-  }, [inView, controls, titleControls, itemsControls]);
+  
 
   return (
     <motion.div
-      ref={(node) => {
-        sectionRef.current = node;
-        inViewRef(node);
-      }}
-      initial={{ opacity: 0 }}
-      animate={controls}
-      className=" relative flex flex-col lg:flex-row gap-10 lg:gap-0 lg:items-center justify-between font-primary   px-4 py-8 lg:p-12 overflow-hidden "
+      
+      className=" relative flex flex-col lg:flex-row    font-primary   px-4 py-8 lg:p-12 overflow-hidden "
     >
       {/* SVG Mask */}
       {/* <img
@@ -98,21 +73,26 @@ const Offer1 = () => {
         className="absolute top left-2 lg:left-[80%] w-1/4 h-auto opacity-40 z-0"
         style={{ transform: 'translate(-10%, -10%)' }}
       /> */}
+      <div className=' w-1/4 px-4 py-6 flex flex-col  gap-8  '>
       <motion.h2
-        ref={sectionRef}
-        initial={{ x: 50, opacity: 0 }}
-        animate={titleControls}
-        className="justify-start text-xl lg:text-2xl text-tertiary-900  font-bold   relative z-10"
+        
+        className="text-xl lg:text-2xl text-tertiary-900  font-bold   relative z-10"
       >
-        What We1 Do ?
+        What We Do ?
       </motion.h2>
-      <div className=" grid grid-cols-2 md:grid-cols-3 gap-8  md:gap-8 relative z-10 one">
+        <p 
+        className='text-sm lg:text-base  text-tertiary-600  '
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex tenetur cupiditate adipisci sint, praesentium et aliquam itaque voluptatum quis!</p>
+
+        {/* <img className='w-10  h-10 lg:w-40  lg:h-40  text-tertiary-800'
+        src={image1} alt="" /> */}
+
+       </div>
+      <div className="w-3/4   grid grid-cols-2 md:grid-cols-2 gap-8  md:gap-8 relative z-10 one">
         {content.map(({title,path,description,icon:Icon}, index) => (
           <motion.div
             key={index}
-            custom={index}
-            initial={{ x: 50, opacity: 0 }}
-            animate={itemsControls}
+            
             whileHover={
                 {scale : 1.1}
             }
@@ -120,22 +100,25 @@ const Offer1 = () => {
                 perspective: 1000, // Add perspective for 3D effect
                 transformStyle: 'preserve-3d', // Preserve child transformations
               }}
-            className="bg-transparent backdrop-blur-2xl flex flex-col justify-start items-start gap-5 two shadow-xl rounded-lg px-4 py-6 cursor-pointer relative  "
+            className="bg-transparent backdrop-blur-2xl flex flex-col justify-start items-start gap-5 two shadow-sm px-4 py-6 cursor-pointer relative  "
+            
+          >
+
+          
+
+            <div className=' flex flex-col items-start gap-4  '>
+            <Icon 
+            strokeWidth={3}
+            className='text-tertiary-900' />
+            <h3 className="text-base lg:text-lg font-semibold  text-tertiary-800 ">{title}</h3>
+            </div>
+            <p className="text-sm lg:text-base  text-tertiary-600  ">{description}</p>
+            <Link
+            className=' rounded-full px-4 py-1 border-2 shadow  text-siteblack'
             onClick={()=>{
               navigate(`/services/${changeToUrl(title)}`)
             }}
-          >
-
-          <Icon
-          strokeWidth={2}
-          className='w-5 h-5 z-30 absolute top-0 right-0 translate-x-1
-          -translate-y-1 text-tertiary-700' />
-
-            <div className=' flex flex-col items-start gap-3 '>
-            {/* <Icon className='text-tertiary-900' /> */}
-            <h3 className="text-xs lg:text-lg font-semibold font-primary text-tertiary-700 ">{title}</h3>
-            </div>
-            {/* <p className="text-xs lg:text-base font-inconsolata text-tertiary-300 ">{description}</p> */}
+            >More Details</Link>
           </motion.div>
         ))}
       </div>
